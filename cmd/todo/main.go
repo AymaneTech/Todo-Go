2ba28316ffedc3ad44b9e6bc0b10c343d15f4192
geek <elmainiaymane03@gmail.com>
2024-06-11 12:27:35 +0100

    create a simple todo list using go

---
+++ cmd/todo/main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"flag"
+	"fmt"
+	"github.com/AymaneTech/todo-go"
+	"io"
+	"os"
+	"strings"
+)
+
+const (
+	todoFile = ".todos.json"
+)
+
+func main() {
+	add := flag.Bool("add", false, "add a task to the list")
+	complete := flag.Int("complete", 0, "mark a todo as completed")
+	del := flag.Int("del", 0, "delete a todo")
+	list := flag.Bool("list", false, "list all todos")
+
+	flag.Parse()
+
+	todos := &todo.Todos{}
+
+	if err := todos.Load(todoFile); err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+
+	switch {
+	case *add:
+		task, err := getInput(os.Stdin, flag.Args()...)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+		todos.Add(task)
+		err = todos.Store(todoFile)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+	case *complete > 0:
+		err := todos.Complete(*complete)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+		err = todos.Store(todoFile)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+	case *del > 0:
+		err := todos.Delete(*del)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+		}
+		err = todos.Store(todoFile)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+		}
+	case *list:
+		todos.Print()
+
+	default:
+		fmt.Fprintln(os.Stdout, "invalid Command")
+		os.Exit(0)
+	}
+}
+
+func getInput(r io.Reader, args ...string) (string, error) {
+	if len(args) > 0 {
+		return strings.Join(args, " "), nil
+	}
+	scanner := bufio.NewScanner(r)
+	scanner.Scan()
+	if err := scanner.Err(); err != nil {
+		return "", err
+	}
+	text := scanner.Text()
+	if len(text) == 0 {
+		return "", errors.New("empty todo is not allowed")
+	}
+	return text, nil
+}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/AymaneTech/todo-go
+
+go 1.21.1
+
+require (
+	github.com/alexeyco/simpletable v1.0.0 // indirect
+	github.com/mattn/go-runewidth v0.0.12 // indirect
+	github.com/rivo/uniseg v0.1.0 // indirect
+)
